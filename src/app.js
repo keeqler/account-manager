08136133859e5dbb350ed50b13e6cb5f82a61e67
Keepler <arturnunespe@gmail.com>
2019-11-04 06:28:47 -0300

    First commit

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+const routes = require('./routes');
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(morgan('dev'));
+    this.server.use(cors());
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
