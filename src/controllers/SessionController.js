105ae1d970dd839575bf19ff02f82318c906ff5b
Keepler <arturnunespe@gmail.com>
2019-11-09 12:13:19 -0300

    Add POST /sessions route

---
+++ src/controllers/SessionController.js
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken';
+
+import User from '@models/User';
+
+class SessionController {
+  async store(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user)
+      return res
+        .status(400)
+        .send({ error: 'Wrong e-mail address or password.' });
+
+    if (!(await user.checkPassword(password)))
+      return res
+        .status(400)
+        .send({ error: 'Wrong e-mail address or password.' });
+
+    return res.send({
+      user: {
+        id: user.id,
+        email,
+      },
+      token: jwt.sign({ id: user.id, email }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRY,
+      }),
+    });
+  }
+}
+
+export default new SessionController();
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 
 import UserController from '@controllers/UserController';
+import SessionController from '@controllers/SessionController';
 
 const router = new Router();
 
+// /users
 router.post('/users', UserController.store);
+
+// /sessions
+router.post('/sessions', SessionController.store);
 
 export default router;
