7b2c4e0a9245676ac4030048b68c545f92f6ce6e
Keepler <arturnunespe@gmail.com>
2019-11-10 04:52:35 -0300

    Create update method for PUT /users route

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -14,7 +14,32 @@ class UserController {
       password: await bcrypt.hash(req.body.password, 10),
     });
 
-    return res.status(201).send({ email: user.dataValues.email });
+    return res.status(201).send({ ...user.dataValues, password: undefined });
+  }
+
+  async update(req, res) {
+    const { password, email, newPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (!(await user.checkPassword(password)))
+      return res.status(400).send({ error: 'Password does not match.' });
+
+    if (email && email !== user.email) {
+      const emailTaken = await User.findOne({
+        where: { email },
+      });
+
+      if (emailTaken)
+        return res.status(400).send({ error: 'E-mail already taken.' });
+
+      user.email = email;
+    }
+    if (newPassword) user.password = await bcrypt.hash(newPassword, 10);
+
+    await user.save();
+
+    return res.send({ ...user.dataValues, password: undefined });
   }
 }
 
