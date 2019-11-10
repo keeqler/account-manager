e71a972bcb60061d29f2c048dd72f55ae27cbc2c
Keepler <arturnunespe@gmail.com>
2019-11-10 04:53:22 -0300

    Add auth middleware and PUT /users route

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,23 @@
 import { Router } from 'express';
 
+import authMiddleware from '@middlewares/auth';
+
 import UserController from '@controllers/UserController';
 import SessionController from '@controllers/SessionController';
 
 const router = new Router();
 
+// --- No auth routes
 // /users
 router.post('/users', UserController.store);
 
 // /sessions
 router.post('/sessions', SessionController.store);
+
+// --- Auth routes
+router.use(authMiddleware);
+
+// /users
+router.put('/users', UserController.update);
 
 export default router;
