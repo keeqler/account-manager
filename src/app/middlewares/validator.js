4cea50545a69aeef4aea14f9466800d708da3754
Keepler <arturnunespe@gmail.com>
2019-11-14 04:36:41 -0300

    Receive schemas for body and params validation

--- src/app/middlewares/validator.js
+++ src/app/middlewares/validator.js
@@ -1,6 +1,10 @@
 export default async (req, res, next) => {
-  if (!(await req.schema.isValid(req.body)))
-    return res.status(400).send({ error: 'Validation failed.' });
+  if (req.bodySchema)
+    if (!(await req.bodySchema.isValid(req.body)))
+      return res.status(400).send({ error: 'Validation failed.' });
+  if (req.paramsSchema)
+    if (!(await req.paramsSchema.isValid(req.params)))
+      return res.status(400).send({ error: 'Validation failed.' });
 
   return next();
 };
