8e9249af46774a379d60cff0278efb73c79305a2
Keepler <arturnunespe@gmail.com>
2019-11-19 08:55:08 -0300

    Revert 278e179

--- src/database/index.js
+++ src/database/index.js
@@ -2,6 +2,8 @@ import Sequelize from 'sequelize';
 
 import User from '@/app/models/User';
 
+import dbConfig from '@/config/database';
+
 const models = [User];
 
 class Database {
@@ -10,18 +12,7 @@ class Database {
   }
 
   init() {
-    this.connection = new Sequelize({
-      dialect: 'postgres',
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      host: process.env.DB_HOST,
-      define: {
-        timestamps: true,
-        underscored: true,
-        underscoredAll: true,
-      },
-    });
+    this.connection = new Sequelize(dbConfig);
 
     models.map(model => model.init(this.connection));
   }
