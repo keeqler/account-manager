e988f8b77c54d6098b558961e2931cac58f778b7
Keepler <arturnunespe@gmail.com>
2020-01-31 00:52:16 -0300

    Fixes #2 Create global stylesheet

---
+++ web/src/components/GlobalStylesheet.js
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto:100,400,700&display=swap');
+
+  :root {
+    --brand-gradient: linear-gradient(to right, #ea3535, #232d45);
+    --primary: #566da5;
+    --neutral-1: #fff;
+    --neutral-2: #d8d8d8;
+    --background-1: #020409;
+    --background-2: #1e1e23;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: none;
+    box-sizing: border-box;
+    font-family: Roboto, sans-serif;
+    color: var(--neutral-1);
+    border: none;
+    background: none;
+  }
+
+  body {
+    font-size: 14px;
+    -webkit-font-smoothing: antialiased;
+    background: var(--background-1);
+  }
+`;
