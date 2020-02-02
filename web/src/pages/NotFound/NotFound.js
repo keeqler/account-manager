a659a697f6274cc1022248f1de6f5f8bd627b37e
Keepler <arturnunespe@gmail.com>
2020-02-02 05:16:30 -0300

    Semantics and styling improvements

--- web/src/pages/NotFound/NotFound.js
+++ web/src/pages/NotFound/NotFound.js
@@ -5,6 +5,6 @@ import Container from './NotFoundStyles';
 export default () => (
   <Container>
     <h1 className="error">Error 404 =(</h1>
-    <h1 className="light">We looked for this page everywhere...</h1>
+    <h2 className="light">We looked for this page everywhere...</h2>
   </Container>
 );
--- web/src/pages/NotFound/NotFoundStyles.js
+++ web/src/pages/NotFound/NotFoundStyles.js
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
 export default styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   text-align: center;
 
   .error {
@@ -9,5 +13,6 @@ export default styled.div`
 
   .light {
     font-weight: 100;
+    font-size: 28px;
   }
 `;
