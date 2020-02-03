6d824694490e90c2aaa078057d14557ce265da16
Keepler <arturnunespe@gmail.com>
2020-02-03 09:40:15 -0300

    Syntax fix on Reactotron config

--- web/src/config/Reactotron.js
+++ web/src/config/Reactotron.js
@@ -1,7 +1,6 @@
 import Reactotron from 'reactotron-react-js';
 
-if (process.env.NODE_ENV === 'development')
-  // eslint-disable-next-line no-console
-  console.tron = Reactotron.configure()
-    .connect()
-    .clear();
+if (process.env.NODE_ENV === 'development') {
+  console.tron = Reactotron.configure();
+  console.tron.connect().clear();
+}
