00803f3f5d515a37e8aa68e0d3139e150297e643
Keepler <arturnunespe@gmail.com>
2020-02-05 09:25:28 -0300

    Fixes #7 Create form component

---
+++ web/src/components/Form.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+import { Form } from '@rocketseat/unform';
+
+export default styled(Form)`
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    margin-bottom: 40px;
+    font-size: 50px;
+    text-align: center;
+  }
+
+  .text-input {
+    margin-bottom: 12px;
+  }
+
+  .submit {
+    margin-top: 24px;
+  }
+
+  .request-message {
+    margin: 6px auto;
+  }
+
+  .last-link-wrapper {
+    font-size: 16px;
+    font-weight: 700;
+    text-align: center;
+  }
+`;
