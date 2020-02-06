03e02419b13f3a5153a586aced6fbb0b1ee63357
Keepler <arturnunespe@gmail.com>
2020-02-06 05:07:47 -0300

    Fixes #9 Create register page form

--- web/src/pages/Register/Register.js
+++ web/src/pages/Register/Register.js
@@ -1,5 +1,69 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import * as Yup from 'yup';
+
+import useRequestMessage from '~/hooks/useRequestMessage';
 
 import Container from './RegisterStyles';
+import Form from '~/components/Form';
+import TextInput from '~/components/TextInput/TextInput';
+import Button from '~/components/Button/Button';
+import RequestMessage from '~/components/RequestMessage/RequestMessage';
 
-export default () => <Container>Register</Container>;
+export default () => {
+  const [requestMessage, setRequestMessage] = useRequestMessage();
+
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  const schema = Yup.object().shape({
+    email: Yup.string()
+      .email('Invalid e-mail address')
+      .required('This field is required'),
+    password: Yup.string()
+      .min(8, 'Password must be at least 8 characters long')
+      .required('This field is required'),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref('password'), null], "Passwords don't match")
+      .required('This field is required'),
+  });
+
+  return (
+    <Container>
+      <Form onSubmit={handleSubmit} schema={schema}>
+        <h1>Register</h1>
+        <TextInput
+          className="text-input"
+          placeholder="E-mail address"
+          name="email"
+        />
+        <TextInput
+          className="text-input password"
+          placeholder="Password"
+          name="password"
+          isPassword
+        />
+        <TextInput
+          className="text-input password"
+          placeholder="Confirm password"
+          name="confirmPassword"
+          isPassword
+        />
+        <Button className="submit" text="Register" isSubmit />
+        <RequestMessage
+          className="request-message"
+          show={requestMessage.show}
+          message={requestMessage.message}
+          isError={requestMessage.isError}
+        />
+        <span className="last-link-wrapper">
+          Already have an account?{' '}
+          <Link className="link" to="/">
+            Login now
+          </Link>
+        </span>
+      </Form>
+    </Container>
+  );
+};
