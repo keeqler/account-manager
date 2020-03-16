50744f09ba7b6a144b00da965e21dcdad6d95eb3
Keepler <arturnunespe@gmail.com>
2020-03-16 12:36:08 -0300

    Make redis client connect to the host specified in REDIS_HOST

--- api/src/lib/RedisClient.js
+++ api/src/lib/RedisClient.js
@@ -1,4 +1,4 @@
 import { createClient } from 'redis';
 import { promisifyAll } from 'bluebird';
 
-export default promisifyAll(createClient());
+export default promisifyAll(createClient(6379, process.env.REDIS_HOST));
